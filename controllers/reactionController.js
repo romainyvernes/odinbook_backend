eaa79303452bbb7b7e524686cb20af98737e0d00
Romain <romainyvernes@gmail.com>
2021-10-18 11:27:09 -0700

    Fill in functions in reactionController.js

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -2,15 +2,35 @@ const Reaction = require('../models/reaction');
 
 // POST add a reaction to a given post
 exports.reactions_add = (req, res, next) => {
-
+  new Reaction({
+    author: req.user.id,
+    parent_id: req.body.parentId,
+    value: req.body.value
+  }).save((err) => {
+    if (err) return next(err);
+    // indicates new post was successfully created
+    res.status(201);
+  });
 };
 
 // PUT update reaction to a given post
 exports.reactions_update = (req, res, next) => {
-
+  Reaction.findByIdAndUpdate(
+    req.params.reactionId, 
+    { value: req.body.value },
+    (err, reaction) => {
+      if (err) return next(err);
+      // indicates update was successful
+      res.status(200);
+    }
+  );
 };
 
 // DELETE delete reaction to a given post
 exports.reactions_delete = (req, res, next) => {
-
+  Reaction.findByIdAndDelete(req.params.reactionId, (err) => {
+    if (err) return next(err);
+    // indicates deletion was successful
+    res.status(200);
+  });
 };
