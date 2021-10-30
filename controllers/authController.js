efca751d3b5ade7ec06cddcef15ed217bd0e7b4d
Romain <romainyvernes@gmail.com>
2021-10-29 21:17:00 -0700

    Add unique email address as condition to create new user account

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,26 +62,34 @@ exports.register = [
 
   (req, res, next) => {
     // check that user is not already registered
-    User.findOne({ username: req.body.username }, (err, user) => {
-      if (err) return next(err);
-      if (user) return res.json({ message: 'User already exists.' });
-
-      // hash password and save new user into DB
-      bcrypt.hash(req.body.password, 10, (err, hashed) => {
+    User.findOne(
+      { $or: [
+          { username: req.body.username },
+          { email: req.body.email }
+        ] 
+      }, 
+      (err, user) => {
         if (err) return next(err);
-        
-        // create new user into DB
-        User.create({
-          username: req.body.username,
-          password: hashed,
-          first_name: req.body.firstName,
-          last_name: req.body.lastName,
-          email: req.body.email
-        }, (err) => {
-          if (err) return next(err);
-          // indicates new user was successfully created
-          res.sendStatus(201);
+        if (user) return res.json({ 
+          message: 'Username or email are already taken.' 
         });
+
+        // hash password and save new user into DB
+        bcrypt.hash(req.body.password, 10, (err, hashed) => {
+          if (err) return next(err);
+          
+          // create new user into DB
+          User.create({
+            username: req.body.username,
+            password: hashed,
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email
+          }, (err) => {
+            if (err) return next(err);
+            // indicates new user was successfully created
+            res.sendStatus(201);
+          });
       });
     });
   }
